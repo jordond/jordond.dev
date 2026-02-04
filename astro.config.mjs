04befb8504c4ab587a4f0eb3c5db9e3e162a1aae
Jordon de Hoog <jordon.dehoog@gmail.com>
2026-02-03 22:20:04 -0500

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://jordond.dev',
+  site: "https://jordond.dev",
   integrations: [sitemap()],
 })
